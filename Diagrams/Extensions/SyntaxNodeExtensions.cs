1da3fdcc8770423c7a2d0fbb1eb2008e85f91a68
Justin Britt <britt@uwindsor.ca>
2021-02-26 09:57:03 -0500

    Added class

---
+++ Diagrams/Extensions/SyntaxNodeExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace Diagrams.Extensions
+{
+    using Microsoft.CodeAnalysis;
+
+    internal static class SyntaxNodeExtensions
+    {
+        public static T GetParent<T>(this SyntaxNode syntaxNode)
+            where T : SyntaxNode
+        {
+            SyntaxNode node = syntaxNode;
+
+            while (node != null && !(node is T))
+                node = node.Parent;
+
+            return node as T;
+        }
+
+        public static bool TryGetParentSyntax<T>(this SyntaxNode syntaxNode, out T result)
+           where T : SyntaxNode
+        {
+            result = syntaxNode.GetParent<T>();
+
+            return result != null;
+        }
+    }
+}
