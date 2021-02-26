1899859e4f50e83876750ab97fbd97ea75d5dad3
Justin Britt <britt@uwindsor.ca>
2021-02-26 09:58:39 -0500

    Added class

---
+++ Diagrams/Extensions/StringExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Diagrams.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal static class StringExtensions
+    {
+        public static string RemoveNewLines(this string stringWithNewLines, bool cleanWhitespace = false)
+        {
+            List<char> splitElementList = cleanWhitespace ? new List<char> { '\r', '\n', ' ' } : new List<char> { '\r', '\n' };
+
+            string[] stringElements = stringWithNewLines.Split(splitElementList.ToArray(), StringSplitOptions.RemoveEmptyEntries);
+            
+            return stringElements.Any() ? string.Join(" ", stringElements) : stringWithNewLines;
+        }
+    }
+}
