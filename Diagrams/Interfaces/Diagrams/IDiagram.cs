fb69427c55bee39195e2f28753b77a41b0dbbc23
Justin Britt <britt@uwindsor.ca>
2021-02-26 11:28:35 -0500

    Added interface

---
+++ Diagrams/Interfaces/Diagrams/IDiagram.cs
@@ -0,0 +1,15 @@
+﻿namespace DotNetDiagrams.Interfaces.Diagrams
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IDiagram
+    {
+        string Title { get; set; }
+
+        List<string> Code { get; set; }
+    }
+}
