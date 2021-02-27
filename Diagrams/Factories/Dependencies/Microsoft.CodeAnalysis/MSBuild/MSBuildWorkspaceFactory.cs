876be230b4f779a75bd8930c3d2decad4a568f44
Justin Britt <britt@uwindsor.ca>
2021-02-27 11:30:54 -0500

    Added factory

---
+++ Diagrams/Factories/Dependencies/Microsoft.CodeAnalysis/MSBuild/MSBuildWorkspaceFactory.cs
@@ -0,0 +1,27 @@
+﻿namespace DotNetDiagrams.Factories.Dependencies.Microsoft.CodeAnalysis.MSBuild
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using global::Microsoft.Build.Locator;
+    using global::Microsoft.CodeAnalysis.MSBuild;
+
+    using global::DotNetDiagrams.InterfacesFactories.Dependencies.Microsoft.CodeAnalysis.MSBuild;
+
+    internal sealed class MSBuildWorkspaceFactory : IMSBuildWorkspaceFactory
+    {
+        public MSBuildWorkspaceFactory()
+        {
+        }
+
+        public MSBuildWorkspace CreateAndRegisterDefaults()
+        {
+            MSBuildLocator.RegisterDefaults();
+
+            return MSBuildWorkspace.Create();
+        }
+    }
+}
