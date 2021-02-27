96eb157c66f54c403b4402ed76212134c1ddf743
Justin Britt <britt@uwindsor.ca>
2021-02-27 11:31:30 -0500

    Changes to usings

--- Diagrams/Program.cs
+++ Diagrams/Program.cs
@@ -9,9 +9,11 @@ namespace DotNetDiagrams
     using Microsoft.CodeAnalysis;
     using Microsoft.CodeAnalysis.MSBuild;
 
+    using DotNetDiagrams.Factories.Dependencies.Microsoft.CodeAnalysis.MSBuild;
     using DotNetDiagrams.Factories.Generators;
     using DotNetDiagrams.Interfaces.Diagrams;
     using DotNetDiagrams.Interfaces.Generators;
+    using DotNetDiagrams.InterfacesFactories.Dependencies.Microsoft.CodeAnalysis.MSBuild;
     using DotNetDiagrams.InterfacesFactories.Generators;
 
     internal sealed class Program
