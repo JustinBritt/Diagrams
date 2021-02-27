53786a2f03cc38454c9629ccb7db16040cc0de79
Justin Britt <britt@uwindsor.ca>
2021-02-27 14:36:41 -0500

    Moved code

--- Diagrams/Classes/Walkers/PlantUMLWalker.cs
+++ Diagrams/Classes/Walkers/PlantUMLWalker.cs
@@ -236,34 +236,39 @@
 
         private void Visit(MemberAccessExpressionSyntax invocation)
         {
+            string callerTypeName;
+            
+            SemanticModel semanticModel;
+
+            MethodDeclarationSyntax methodHost = invocation.GetParent<MethodDeclarationSyntax>();
+            
+            ConstructorDeclarationSyntax constructorHost = invocation.GetParent<ConstructorDeclarationSyntax>();
+
+            if (methodHost != null)
+            {
+                callerTypeName = methodHost.GetParent<ClassDeclarationSyntax>().Identifier.ValueText;
+                
+                semanticModel = compilation.GetSemanticModel(methodHost.SyntaxTree, true);
+            }
+            else if (constructorHost != null)
+            {
+                callerTypeName = constructorHost.GetParent<ClassDeclarationSyntax>().Identifier.ValueText;
+                
+                semanticModel = compilation.GetSemanticModel(constructorHost.SyntaxTree, true);
+            }
+            else
+            {
+                base.Visit(invocation);
+
+                return;
+            }
+
+            string targetTypeName;
+            string targetName;
+            string returnTypeName;
+
             if (invocation.Expression is IdentifierNameSyntax identifierName)
             {
-                string callerTypeName;
-                SemanticModel semanticModel;
-
-                MethodDeclarationSyntax methodHost = invocation.GetParent<MethodDeclarationSyntax>();
-                ConstructorDeclarationSyntax constructorHost = invocation.GetParent<ConstructorDeclarationSyntax>();
-
-                if (methodHost != null)
-                {
-                    callerTypeName = methodHost.GetParent<ClassDeclarationSyntax>().Identifier.ValueText;
-                    semanticModel = compilation.GetSemanticModel(methodHost.SyntaxTree, true);
-                }
-                else if (constructorHost != null)
-                {
-                    callerTypeName = constructorHost.GetParent<ClassDeclarationSyntax>().Identifier.ValueText;
-                    semanticModel = compilation.GetSemanticModel(constructorHost.SyntaxTree, true);
-                }
-                else
-                {
-                    base.Visit(invocation);
-                    return;
-                }
-
-                string targetTypeName;
-                string targetName;
-                string returnTypeName;
-
                 if (ModelExtensions.GetTypeInfo(semanticModel, identifierName).Type == null)
                 {
                     // same type as caller
