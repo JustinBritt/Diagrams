616aa64cce0d3123171c52069f3602c9f1d67b93
Justin Britt <britt@uwindsor.ca>
2021-03-01 13:41:26 -0500

    Removed unused usings

--- DotNetDiagrams/Classes/Generators/PlantUMLDiagramGenerator.cs
+++ DotNetDiagrams/Classes/Generators/PlantUMLDiagramGenerator.cs
@@ -1,16 +1,11 @@
 ﻿namespace DotNetDiagrams.Classes.Generators
 {
-    using System.Collections.Generic;
-
-    using Microsoft.Build.Locator;
     using Microsoft.CodeAnalysis;
-    using Microsoft.CodeAnalysis.MSBuild;
 
     using DotNetDiagrams.Classes.Diagrams;
     using DotNetDiagrams.Classes.Walkers;
     using DotNetDiagrams.Interfaces.Diagrams;
     using DotNetDiagrams.Interfaces.Generators;
-    using DotNetDiagrams.Interfaces.Walkers;
 
     internal sealed class PlantUMLDiagramGenerator : IDiagramGenerator
     {
