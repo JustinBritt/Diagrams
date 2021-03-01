204d7303fa700b96133f9994df8706aed7379923
Justin Britt <britt@uwindsor.ca>
2021-03-01 13:56:48 -0500

    Added class

---
+++ DotNetDiagrams/Classes/Configurations/PlantUMLSequenceDiagramConfiguration.cs
@@ -0,0 +1,21 @@
+﻿namespace DotNetDiagrams.Classes.Configurations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using DotNetDiagrams.Interfaces.Configurations;
+
+    internal sealed class PlantUMLSequenceDiagramConfiguration : IPlantUMLSequenceDiagramConfiguration
+    {
+        public PlantUMLSequenceDiagramConfiguration()
+        {
+        }
+
+        public bool Autoactivate { get; }
+
+        public bool Footbox { get; }
+    }
+}
---
+++ DotNetDiagrams/Interfaces/Configurations/IPlantUMLSequenceDiagramConfiguration.cs
@@ -0,0 +1,12 @@
+﻿namespace DotNetDiagrams.Interfaces.Configurations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IPlantUMLSequenceDiagramConfiguration
+    {
+    }
+}
