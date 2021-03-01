41a036857d4d96562f1cf3e77331b9018f5faea1
Justin Britt <britt@uwindsor.ca>
2021-03-01 15:01:03 -0500

    Added factory

---
+++ DotNetDiagrams.Common/Factories/Diagrams/PlantUMLDiagramFactory.cs
@@ -0,0 +1,38 @@
+﻿namespace DotNetDiagrams.Common.Factories.Diagrams
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using DotNetDiagrams.Common.Classes.Diagrams;
+    using DotNetDiagrams.Common.Interfaces.Diagrams;
+    using DotNetDiagrams.Common.InterfacesFactories.Diagrams;
+
+    internal sealed class PlantUMLDiagramFactory : IPlantUMLDiagramFactory
+    {
+        public PlantUMLDiagramFactory()
+        {
+        }
+
+        public IDiagram Create(
+            string title,
+            List<string> code)
+        {
+            IPlantUMLDiagram instance = null;
+
+            try
+            {
+                instance = new PlantUMLDiagram(
+                    title,
+                    code);
+            }
+            catch (Exception exception)
+            {
+            }
+
+            return instance;
+        }
+    }
+}
---
+++ DotNetDiagrams.Common/InterfacesFactories/Diagrams/IPlantUMLDiagramFactory.cs
@@ -0,0 +1,17 @@
+﻿namespace DotNetDiagrams.Common.InterfacesFactories.Diagrams
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using DotNetDiagrams.Common.Interfaces.Diagrams;
+
+    public interface IPlantUMLDiagramFactory
+    {
+        IDiagram Create(
+            string title,
+            List<string> code);
+    }
+}
