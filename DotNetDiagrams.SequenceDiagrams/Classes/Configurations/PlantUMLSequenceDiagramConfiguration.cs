07d978536bf8206de4ac19e7cd9f2b71f900882e
Justin Britt <britt@uwindsor.ca>
2021-03-01 14:26:07 -0500

    Added projects

---
+++ DotNetDiagrams.SequenceDiagrams/Classes/Configurations/PlantUMLSequenceDiagramConfiguration.cs
@@ -0,0 +1,26 @@
+﻿namespace DotNetDiagrams.SequenceDiagrams.Classes.Configurations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using DotNetDiagrams.SequenceDiagrams.Interfaces.Configurations;
+
+    internal sealed class PlantUMLSequenceDiagramConfiguration : IPlantUMLSequenceDiagramConfiguration
+    {
+        public PlantUMLSequenceDiagramConfiguration(
+            bool autoactivate = true,
+            bool footbox = true)
+        {
+            this.Autoactivate = autoactivate;
+
+            this.Footbox = footbox;
+        }
+
+        public bool Autoactivate { get; }
+
+        public bool Footbox { get; }
+    }
+}
