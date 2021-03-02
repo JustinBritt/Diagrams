cce3eee7b9d077cf9d8768eb8e46c49bbea73f00
Justin Britt <britt@uwindsor.ca>
2021-03-02 08:36:16 -0500

    Changes to usings

--- DotNetDiagrams.ClassDiagrams/Classes/Walkers/PlantUMLClassDiagramWalker.cs
+++ DotNetDiagrams.ClassDiagrams/Classes/Walkers/PlantUMLClassDiagramWalker.cs
@@ -6,6 +6,14 @@
     using System.Text;
     using System.Threading.Tasks;
 
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using Microsoft.CodeAnalysis.FindSymbols;
+
+    using DotNetDiagrams.Common.Classes.Diagrams;
+    using DotNetDiagrams.Common.Extensions;
+    using DotNetDiagrams.Common.Interfaces.Diagrams;
     using DotNetDiagrams.ClassDiagrams.Interfaces.Walkers;
 
     internal sealed class PlantUMLClassDiagramWalker : IPlantUMLClassDiagramWalker
