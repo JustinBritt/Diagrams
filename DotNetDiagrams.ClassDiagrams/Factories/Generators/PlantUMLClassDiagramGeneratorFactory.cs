fd34162d7770fd4f86ca3564a6ea8123441a4510
Justin Britt <britt@uwindsor.ca>
2021-03-02 13:17:56 -0500

    Added factory

---
+++ DotNetDiagrams.ClassDiagrams/Factories/Generators/PlantUMLClassDiagramGeneratorFactory.cs
@@ -0,0 +1,30 @@
+﻿namespace DotNetDiagrams.ClassDiagrams.Factories.Generators
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using DotNetDiagrams.ClassDiagrams.Classes.Generators;
+    using DotNetDiagrams.ClassDiagrams.Interfaces.Generators;
+    using DotNetDiagrams.ClassDiagrams.InterfacesFactories.Generators;
+
+    internal sealed class PlantUMLClassDiagramGeneratorFactory : IPlantUMLClassDiagramGeneratorFactory
+    {
+        public IClassDiagramGenerator Create()
+        {
+            IClassDiagramGenerator instance = null;
+
+            try
+            {
+                instance = new PlantUMLClassDiagramGenerator();
+            }
+            catch (Exception exception)
+            {
+            }
+
+            return instance;
+        }
+    }
+}
