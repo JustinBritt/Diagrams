407629afb30e8bfe22a4828f7a2fa60cf20a207d
Justin Britt <britt@uwindsor.ca>
2021-03-02 00:18:32 -0500

    Added class

---
+++ DotNetDiagrams.ClassDiagrams/Classes/Generators/PlantUMLClassDiagramGenerator.cs
@@ -0,0 +1,17 @@
+﻿namespace DotNetDiagrams.ClassDiagrams.Classes.Generators
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using DotNetDiagrams.ClassDiagrams.Interfaces.Generators;
+
+    internal sealed class PlantUMLClassDiagramGenerator : IPlantUMLClassDiagramGenerator
+    {
+        public PlantUMLClassDiagramGenerator()
+        {
+        }
+    }
+}
