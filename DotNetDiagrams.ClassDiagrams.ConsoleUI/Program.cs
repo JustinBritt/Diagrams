02fbdf9484b501a4540b9869a835a5e430523a45
Justin Britt <britt@uwindsor.ca>
2021-03-02 12:55:38 -0500

    Added project

---
+++ DotNetDiagrams.ClassDiagrams.ConsoleUI/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace DotNetDiagrams.ClassDiagrams.ConsoleUI
+{
+    using System;
+
+    internal sealed class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
