58b3dab40360aefb888d23540ad7504dd6235a34
Justin Britt <britt@uwindsor.ca>
2021-03-02 08:31:56 -0500

    Added class

---
+++ DotNetDiagrams.ClassDiagrams/Classes/Walkers/PlantUMLClassDiagramWalker.cs
@@ -0,0 +1,17 @@
+﻿namespace DotNetDiagrams.ClassDiagrams.Classes.Walkers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using DotNetDiagrams.ClassDiagrams.Interfaces.Walkers;
+
+    internal sealed class PlantUMLClassDiagramWalker : IPlantUMLClassDiagramWalker
+    {
+        public PlantUMLClassDiagramWalker()
+        {
+        }
+    }
+}
