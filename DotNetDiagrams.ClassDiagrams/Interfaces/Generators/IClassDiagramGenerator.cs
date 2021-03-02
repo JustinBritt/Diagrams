054af682dce9747e48fa17d2f7d1632db8f0e2ea
Justin Britt <britt@uwindsor.ca>
2021-03-02 13:20:37 -0500

    Added interface

---
+++ DotNetDiagrams.ClassDiagrams/Interfaces/Generators/IClassDiagramGenerator.cs
@@ -0,0 +1,12 @@
+﻿namespace DotNetDiagrams.ClassDiagrams.Interfaces.Generators
+{
+    using Microsoft.CodeAnalysis;
+
+    using DotNetDiagrams.Common.Interfaces.Diagrams;
+
+    public interface IClassDiagramGenerator
+    {
+        IDiagrams Process(
+            Solution solution);
+    }
+}
