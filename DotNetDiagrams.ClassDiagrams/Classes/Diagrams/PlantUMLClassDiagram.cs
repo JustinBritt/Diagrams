617ad12c51fe90c292a72b93ef46f5002510bc7e
Justin Britt <britt@uwindsor.ca>
2021-03-05 15:24:46 -0500

    Removed unused code

--- DotNetDiagrams.ClassDiagrams/Classes/Diagrams/PlantUMLClassDiagram.cs
+++ DotNetDiagrams.ClassDiagrams/Classes/Diagrams/PlantUMLClassDiagram.cs
@@ -21,11 +21,7 @@
 
         public List<string> Code { get; set; }
 
-        public string End { get; set; }
-
         public List<string> Header { get; set; }
-
-        public string Start { get; set; }
 
         public string Title { get; set; }
 
