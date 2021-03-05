64069ecaff6559d1dd9e6d99f38709ecb6263496
Justin Britt <britt@uwindsor.ca>
2021-03-05 15:45:43 -0500

    Added class

---
+++ DotNetDiagrams.ClassDiagrams/Classes/Diagrams/PlantUMLClassDiagrams.cs
@@ -0,0 +1,44 @@
+﻿namespace DotNetDiagrams.ClassDiagrams.Classes.Diagrams
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using DotNetDiagrams.ClassDiagrams.Interfaces.Diagrams;
+    using DotNetDiagrams.Common.Interfaces.Diagrams;
+
+    internal sealed class PlantUMLClassDiagrams : IPlantUMLClassDiagrams
+    {
+        public PlantUMLClassDiagrams()
+        {
+        }
+
+        public List<IDiagram> Value { get; set; }
+
+        public void AddTitle(
+            string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ContainsTitle(
+            string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetCodeAtTitleOrDefault(
+            string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveAtTitle(
+            string title)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
