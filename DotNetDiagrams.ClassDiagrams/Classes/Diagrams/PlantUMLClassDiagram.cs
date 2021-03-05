26190410faf78a9d6b272a077e136ddf6e440b1a
Justin Britt <britt@uwindsor.ca>
2021-03-05 15:38:11 -0500

    Added properties

--- DotNetDiagrams.ClassDiagrams/Classes/Diagrams/PlantUMLClassDiagram.cs
+++ DotNetDiagrams.ClassDiagrams/Classes/Diagrams/PlantUMLClassDiagram.cs
@@ -26,7 +26,11 @@
 
         public List<string> Code { get; set; }
 
+        public string End { get; set; }
+
         public List<string> Header { get; set; }
+
+        public string Start { get; set; }
 
         public string Title { get; set; }
 
