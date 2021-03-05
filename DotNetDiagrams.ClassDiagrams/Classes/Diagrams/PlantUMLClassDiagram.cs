bec4d6de7e3906729ecfc7d613c893ea72d3c253
Justin Britt <britt@uwindsor.ca>
2021-03-05 15:14:19 -0500

    Added class

---
+++ DotNetDiagrams.ClassDiagrams/Classes/Diagrams/PlantUMLClassDiagram.cs
@@ -0,0 +1,21 @@
+﻿namespace DotNetDiagrams.ClassDiagrams.Classes.Diagrams
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using DotNetDiagrams.ClassDiagrams.Interfaces.Diagrams;
+
+    internal sealed class PlantUMLClassDiagram : IPlantUMLClassDiagram
+    {
+        public PlantUMLClassDiagram()
+        {
+        }
+
+        public string Title { get; set; }
+
+        public List<string> Code { get; set; }
+    }
+}
---
+++ DotNetDiagrams.ClassDiagrams/Interfaces/Diagrams/IPlantUMLClassDiagram.cs
@@ -0,0 +1,14 @@
+﻿namespace DotNetDiagrams.ClassDiagrams.Interfaces.Diagrams
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using DotNetDiagrams.Common.Interfaces.Diagrams;
+
+    public interface IPlantUMLClassDiagram : IPlantUMLDiagram
+    {
+    }
+}
