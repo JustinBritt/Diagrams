20cc6b4524280d1eaf51a3166f0aa044d601e079
Justin Britt <britt@uwindsor.ca>
2021-03-10 15:19:49 -0500

    Added cases

--- DotNetDiagrams.ClassDiagrams/Classes/Walkers/PlantUMLClassDiagramWalker.cs
+++ DotNetDiagrams.ClassDiagrams/Classes/Walkers/PlantUMLClassDiagramWalker.cs
@@ -1073,7 +1073,15 @@
             {
                 string PlantUMLModifier = CSharpModifier switch
                 {
+                    "internal" => stereotype_internal,
+
+                    "private" => stereotype_private,
+
+                    "protected" => stereotype_protected,
+
                     "public" => stereotype_public,
+
+                    "static" => modifier_static,
 
                     _ => throw new Exception("")
                 };
