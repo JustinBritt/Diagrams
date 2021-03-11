d23c8db2be2804a8e3a6af72f241a891711e2bc5
Justin Britt <britt@uwindsor.ca>
2021-03-11 06:11:28 -0500

    Added case

---
+++ DotNetDiagrams.UseCaseDiagrams/Classes/Diagrams/PlantUMLUseCaseDiagram.cs
@@ -0,0 +1,17 @@
+﻿namespace DotNetDiagrams.UseCaseDiagrams.Classes.Diagrams
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using DotNetDiagrams.UseCaseDiagrams.Interfaces.Diagrams;
+
+    internal sealed class PlantUMLUseCaseDiagram : IPlantUMLUseCaseDiagram
+    {
+        public PlantUMLUseCaseDiagram()
+        {
+        }
+    }
+}
---
+++ DotNetDiagrams.UseCaseDiagrams/Interfaces/Diagrams/IPlantUMLUseCaseDiagram.cs
@@ -0,0 +1,12 @@
+﻿namespace DotNetDiagrams.UseCaseDiagrams.Interfaces.Diagrams
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IPlantUMLUseCaseDiagram
+    {
+    }
+}
