c3cc1f6342965605e709ac1dcc3e7e114705058b
Justin Britt <britt@uwindsor.ca>
2021-03-11 06:15:34 -0500

    Implement properties

--- DotNetDiagrams.UseCaseDiagrams/Classes/Diagrams/PlantUMLUseCaseDiagram.cs
+++ DotNetDiagrams.UseCaseDiagrams/Classes/Diagrams/PlantUMLUseCaseDiagram.cs
@@ -5,7 +5,7 @@
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
-
+    using DotNetDiagrams.Common.Interfaces.Diagrams;
     using DotNetDiagrams.UseCaseDiagrams.Interfaces.Diagrams;
 
     internal sealed class PlantUMLUseCaseDiagram : IPlantUMLUseCaseDiagram
@@ -13,5 +13,17 @@
         public PlantUMLUseCaseDiagram()
         {
         }
+
+        public List<string> Body { get; set; }
+        
+        public string End { get; set; }
+
+        public List<string> Header { get; set; }
+        
+        public string Start { get; set; }
+
+        public List<string> Code { get; set; }
+
+        public string Title { get; set; }
     }
 }
