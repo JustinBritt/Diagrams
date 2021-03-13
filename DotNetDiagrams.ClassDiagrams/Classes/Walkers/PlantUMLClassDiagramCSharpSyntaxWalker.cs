abb2142ea16007fe29ae2f0366a09923bb485f3f
Justin Britt <britt@uwindsor.ca>
2021-03-13 10:10:21 -0500

    Added const

--- DotNetDiagrams.ClassDiagrams/Classes/Walkers/PlantUMLClassDiagramCSharpSyntaxWalker.cs
+++ DotNetDiagrams.ClassDiagrams/Classes/Walkers/PlantUMLClassDiagramCSharpSyntaxWalker.cs
@@ -49,6 +49,7 @@
 
         private const string stringJoinSeparator_accessors = " ";
         private const string stringJoinSeparator_baseListTypes = ",";
+        private const string stringJoinSeparator_typeParameters = ", ";
 
         private const string PlantUML_abstract = "abstract";
         private const string PlantUML_anchor = "+--";
