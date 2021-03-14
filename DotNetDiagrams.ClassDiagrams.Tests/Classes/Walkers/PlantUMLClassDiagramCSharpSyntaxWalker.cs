cbf8cdeb7903968f8f8685e026d6287f2bd5e0b7
Justin Britt <britt@uwindsor.ca>
2021-03-13 23:52:51 -0500

    Added class

---
+++ DotNetDiagrams.ClassDiagrams.Tests/Classes/Walkers/PlantUMLClassDiagramCSharpSyntaxWalker.cs
@@ -0,0 +1,13 @@
+﻿namespace DotNetDiagrams.ClassDiagrams.Tests.Classes.Walkers
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class PlantUMLClassDiagramCSharpSyntaxWalker
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
