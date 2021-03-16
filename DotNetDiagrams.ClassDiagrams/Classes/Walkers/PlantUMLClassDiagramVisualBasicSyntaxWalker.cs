a05927a9a512b76dc6d089884a66e9b276f0660c
Justin Britt <britt@uwindsor.ca>
2021-03-16 13:19:34 -0400

    Added const

--- DotNetDiagrams.ClassDiagrams/Classes/Walkers/PlantUMLClassDiagramVisualBasicSyntaxWalker.cs
+++ DotNetDiagrams.ClassDiagrams/Classes/Walkers/PlantUMLClassDiagramVisualBasicSyntaxWalker.cs
@@ -16,6 +16,7 @@
 
     internal sealed class PlantUMLClassDiagramVisualBasicSyntaxWalker : VisualBasicSyntaxWalker, IPlantUMLClassDiagramVisualBasicSyntaxWalker
     {
+        private const string stereotype_internal = "<<internal>>";
         private const string stereotype_protected = "<<protected>>";
         private const string stereotype_public = "<<public>>";
 
